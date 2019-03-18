34fa7688b0a1471aa54bbdf00ecd6c7bb05c1dfe
micha <bagrowski.michal@gmail.com>
2019-03-18 21:28:24 +0100

    Make a Dice app.

---
+++ app/src/main/java/com/example/dice_kotlin/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.dice_kotlin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
