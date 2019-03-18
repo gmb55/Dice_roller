af15469d82a614b2aedd586a24581c9ed637ed80
micha <bagrowski.michal@gmail.com>
2019-03-18 22:12:35 +0100

    Add val "rollButton"

--- app/src/main/java/com/example/dice_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/dice_kotlin/MainActivity.kt
@@ -2,11 +2,16 @@ package com.example.dice_kotlin
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById<View>(R.id.roll_button)
+        rollButton.text = "Let's roll!"
     }
 }
