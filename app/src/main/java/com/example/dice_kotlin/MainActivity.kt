479abfd9405588ebdc6cb08c5205927cf348fda7
micha <bagrowski.michal@gmail.com>
2019-03-18 22:56:05 +0100

    Remove buggs and add correct Toast

--- app/src/main/java/com/example/dice_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/dice_kotlin/MainActivity.kt
@@ -5,17 +5,21 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
+import android.widget.Toast.*
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.view.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val rollButton: Button = findViewById<View>(R.id.roll_button)
-        rollButton.text = "Let's roll!"
-        rollButton.setOnClickListener(
-            Toast.makeText(this, "Button clicked", Toast.LENGTH_SHORT).show()
-        )
+        val rollButton: Button = findViewById(R.id.roll_button)
+            rollButton.setOnClickListener {
+                Toast.makeText(this, "Button clicked", Toast.LENGTH_SHORT).show()
+            }
     }
 }
+
+
+
