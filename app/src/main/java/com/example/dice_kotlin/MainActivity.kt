f898bdccb7334984649bf807229784d0a2fa07aa
micha <bagrowski.michal@gmail.com>
2019-03-19 19:38:13 +0100

    Add function to change Image by Random.

--- app/src/main/java/com/example/dice_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/dice_kotlin/MainActivity.kt
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import android.widget.Toast.*
@@ -23,9 +24,20 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun rollDice() {
-        val resultText: TextView = findViewById(R.id.result_text)
+
         val randomInt = java.util.Random().nextInt(6) + 1
-        resultText.text = randomInt.toString()
+
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        val diceImage: ImageView = findViewById(R.id.dice_image)
+        diceImage.setImageResource(drawableResource)
     }
 }
 
